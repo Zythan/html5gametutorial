eff18c8e2f656b297dc2f82695edfb9bcd90e1b8
Zythan <james@floorsoft.com>
2022-02-20 20:53:51 -0500

    First Commit

---
+++ Untitled-1.js
@@ -0,0 +1,9 @@
+//set font, color, transparance of canvas..??
+// ctx.font      = '30px Arial';
+// ctx.fillStyle = 'red';    
+// ctx.globalAlph = 0.5;    
+// //Text to canvas..??
+// ctx.fillText('Hello',50,50);
+// //Draw to canvas..??
+// ctx.fillRect(50,50,100,100);
+// ctx.clearRect(75,75,100,100);
